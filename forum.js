9563915abae0706268c3a0e04e5ffee8c54fe662
William <122514675+Wilcun@users.noreply.github.com>
2025-06-09 03:54:14 +0700

    Add files via upload

---
+++ forum.js
@@ -0,0 +1,4 @@
+    // Sederhanakan sidebar toggle untuk halaman ini
+    document.querySelector('.toggle-sidebar-btn').addEventListener('click', function() {
+      document.querySelector('.sidebar').classList.toggle('collapsed');
+    });
